a8cc6f75f5f931c2f3c466339b0b67e79a0bfe87
raghav <raghavpaliwal1109@gmail.com>
2024-08-04 19:02:46 +0530

    special project

--- final code.py
+++ final code.py
@@ -24,28 +24,21 @@ def star_func():
     else:
         engine.say("sorry but i cannot proceed")
 
-def star_func():
-    info = entry2.get()
-    if info == "open prime":
+def go_func():
+    infor = entry2.get()
+    if infor == "open prime":
         webbrowser.open("https://www.primevideo.com/")
     else:
-        engine.say("sorry i cannot proceed")
-
-def star_func():
-    info = entry2.get()
-    if info == "open extramarks":
-        webbrowser.open("https://www.extramarks.com/")
-    else:
-        engine.say("sorry but i cannot proceed")
+        engine.say ("sorry but i cannot proceed")
         
 root = tk.Tk()
 root.title("friday")
 
-root.geometry("500x800")
+root.geometry("2000x800")
 
-root.config(bg="black")
+root.config(bg="light blue")
 
-label1 = tk.Label(root,text = "friday",height = 2,width=10,font=50,bg="black",fg="white")
+label1 = tk.Label(root,text = "friday",height = 2,width=10,font=50,bg="white",fg="black")
 label1.pack(pady=20)
 
 entry1 = tk.Entry(root, width=50)
@@ -57,8 +50,10 @@ entry2.pack(pady=110)
 button= tk.Button(root,text="start function",command=speak_text,height=5,width=10)
 button.pack(pady=40)
 
-button2 = tk.Button(root,text = "go func",command = star_func,height = 5, width = 10)
-button2.pack(pady=60)
+button2 = tk.Button(root,text = "to youtube",command = star_func,height = 5, width = 10)
+button2.pack(pady=60,side=tk.LEFT, padx=10)
 
+button3 = tk.Button(root, text = "to prime",command = go_func,height= 5,width = 10)
+button3.pack(pady = 60,side = tk.RIGHT,padx= 10)
 root.mainloop()
 
